eecf2df6a2b1ef2e89bddfedf923334b0603cb61
LamTran <lamtt@beetsoft.com.vn>
2019-04-08 23:18:16 +0700

    send email with nodemailer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 console.log('Using Promise to read Json File in Node.js')
-
 let fileManager = require('./fileManager')
+const nodeMailer = require('nodemailer')
+const transporter = nodeMailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'xxx@gmail.com',
+        pass: 'xxx' // type your pass word
+    }
+
+})
 fileManager.buyAnIphone('Ip X').then((response) => {
     console.log(response)
 }).catch((err) => {
@@ -9,4 +17,17 @@ fileManager.buyAnIphone('Ip X').then((response) => {
 
 fileManager.readJsonFile('./src/data.json').then( (jsonObject) => {
     console.log(`Email list = ${jsonObject['emails']}`)
+    let mailOptions = {
+        from: 'xxx@gmail.com',
+        to: jsonObject['emails'],
+        subject: 'Sending email using Nodejs',
+        text: 'this is email sending by Nodejs with nodema  iler'
+    }
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(`Email send : ${info.response}`)
+        }
+    })
 })
